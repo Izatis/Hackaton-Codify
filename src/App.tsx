173977f5313d46f2d52b31f5834e713f0be89e2f
Izatis <mekinovizat1@gmail.com>
2023-06-17 15:14:29 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from 'widgets/app-bar/app-bar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
